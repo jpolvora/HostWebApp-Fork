b465886b6e4ad46948181fbc606ecb498b0f44ac
jpolvora <jpolvora@gmail.com>
2014-09-01 15:37:26 -0400

    scripts

--- init_cfg_subtree.sh
+++ init_cfg_subtree.sh
@@ -1,3 +1,6 @@
+#Allows sync from original repo from this fork
+git remote add hostwebapp https://jpolvora@bitbucket.org/jpolvora/hostwebapp.git 
+
 # Adiciona a subtree p/ pull e atualiza
 git remote add --fetch MvcLib https://jpolvora@bitbucket.org/jpolvora/mvcfromdb.git
 git subtree add --prefix=MvcLib MvcLib master --squash
