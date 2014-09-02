e22ea23b12028cfda6964d526fddb58d355066ea
jpolvora <jpolvora@gmail.com>
2014-09-02 09:45:55 -0400

    migrations

---
+++ HostWebApp/Migrations/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcLib.DbFileSystem;
+
+namespace HostWebApp.Migrations
+{
+    public class Configuration : DbFileContextMigrationConfiguration
+    {
+        public Configuration()
+        {
+            ContextKey = GetType().BaseType.Name;
+        }
+    }
+}
