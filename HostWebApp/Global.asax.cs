f7f57af8d6ace25ae3db00bb10072667d4362898
jpolvora <jpolvora@gmail.com>
2014-09-04 07:31:15 -0400

    tratar a string

--- HostWebApp/Global.asax.cs
+++ HostWebApp/Global.asax.cs
@@ -2,7 +2,6 @@
 using System.Diagnostics;
 using System.IO;
 using System.Web;
-using System.Web.Hosting;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -12,8 +11,14 @@ namespace HostWebApp
 {
     public class Global : System.Web.HttpApplication
     {
+        private static string _rewriteBasePath;
+
         protected void Application_Start(object sender, EventArgs e)
         {
+            _rewriteBasePath = Config.ValueOrDefault("DumpToLocalFolder", "~/dbfiles").TrimEnd('/');
+            if (!_rewriteBasePath.StartsWith("~"))
+                _rewriteBasePath = "~" + _rewriteBasePath;
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
@@ -29,17 +34,15 @@ namespace HostWebApp
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
-            var cfgFolder = Config.ValueOrDefault("DumpToLocalFolder", "~/dbfiles");
-
             string path = Request.Url.AbsolutePath;
 
             if (path == "/")
                 path = "/index.cshtml";
 
-            if (path.StartsWith(cfgFolder.Substring(1)))
+            if (path.StartsWith(_rewriteBasePath.Substring(1)))
                 return;
 
-            string virtualPath = string.Format("{0}{1}", cfgFolder, path);
+            string virtualPath = string.Format("{0}{1}", _rewriteBasePath, path);
 
             if (string.IsNullOrWhiteSpace(VirtualPathUtility.GetFileName(virtualPath)))
             {
@@ -50,7 +53,7 @@ namespace HostWebApp
 
             if (!File.Exists(physicalPath)) return;
 
-            string newpath = string.Format("{0}{1}", cfgFolder.Substring(1), path);
+            string newpath = string.Format("{0}{1}", _rewriteBasePath.Substring(1), path);
 
             Trace.TraceInformation("Rewriting path from '{0}' to '{1}", path, newpath);
             Context.RewritePath(newpath);
