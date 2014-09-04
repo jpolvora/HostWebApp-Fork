ba463552e357c0d74f32b6e13f5014bcf8d032de
jpolvora <jpolvora@gmail.com>
2014-09-04 18:46:00 -0400

    Squashed 'MvcLib/' changes from 14c20a8..d4188e0
    d4188e0 stopiss config
    233901f introducing config section
    
    git-subtree-dir: MvcLib
    git-subtree-split: d4188e09586bdc79d162828cabb2eda9dea157bd

---
+++ MvcLib.Common/Configuration/BootstrapperSection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Configuration;
+
+namespace MvcLib.Common.Configuration
+{
+    public class BootstrapperSection : ConfigurationSection
+    {
+        public static void Initialize()
+        {
+        }
+
+        static BootstrapperSection()
+        {
+            Instance = (BootstrapperSection)ConfigurationManager.GetSection("MvcLib");
+        }
+
+        public static BootstrapperSection Instance { get; private set; }
+
+        [ConfigurationProperty("stopMonitoring", DefaultValue = "false")]
+        public Boolean StopMonitoring
+        {
+            get
+            {
+                return (Boolean)this["stopMonitoring"];
+            }
+            set
+            {
+                this["stopMonitoring"] = value;
+            }
+        }
+
+
+        [ConfigurationProperty("httpModules")]
+        public HttpModulesElement HttpModules
+        {
+            get { return (HttpModulesElement)this["httpModules"]; }
+            set { this["httpModules"] = value; }
+        }
+    }
+}
