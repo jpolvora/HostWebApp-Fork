294527cd0dcdc4aea267e2f8ab6b1625ba0f8a80
jpolvora <jpolvora@gmail.com>
2014-09-03 23:51:28 -0400

    Implementing RewritePath instead of SubdfolderVPP

--- HostWebApp/Global.asax.cs
+++ HostWebApp/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Web;
 using System.Web.Hosting;
 using System.Web.Mvc;
@@ -18,8 +19,6 @@ namespace HostWebApp
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             Trace.TraceInformation("[GLOBAL]: Debugging Enabled: {0}", HttpContext.Current.IsDebuggingEnabled);
-
-            Trace.TraceError("Application_Start...");
         }
 
         protected void Session_Start(object sender, EventArgs e)
@@ -29,7 +28,19 @@ namespace HostWebApp
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
+            string path = Request.Url.AbsolutePath;
 
+            string virtualPath = string.Format("~/dbfiles{0}", path);
+
+            var physicalPath = Server.MapPath(virtualPath);
+
+            if (File.Exists(physicalPath) || Directory.Exists(physicalPath))
+            {
+                string newpath = string.Format("/dbfiles{0}", path);
+
+                Trace.TraceInformation("Rewriting path from '{0}' to '{1}", path, newpath);
+                Context.RewritePath(newpath);
+            }
         }
 
         protected void Application_AuthenticateRequest(object sender, EventArgs e)
