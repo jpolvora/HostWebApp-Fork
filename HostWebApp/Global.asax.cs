ca172de5e32b0ef4e0e4c41bd5a252261f97a0d9
jpolvora <jpolvora@gmail.com>
2014-09-04 00:03:00 -0400

    rules

--- HostWebApp/Global.asax.cs
+++ HostWebApp/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Hosting;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using MvcLib.Common;
 
 namespace HostWebApp
 {
@@ -28,19 +29,31 @@ namespace HostWebApp
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
+            var cfgFolder = Config.ValueOrDefault("DumpToLocalFolder", "~/dbfiles");
+
             string path = Request.Url.AbsolutePath;
 
-            string virtualPath = string.Format("~/dbfiles{0}", path);
+            if (path == "/")
+                path = "/index.cshtml";
+
+            if (path.StartsWith(cfgFolder.Substring(1)))
+                return;
+
+            string virtualPath = string.Format("{0}{1}", cfgFolder, path);
+
+            if (string.IsNullOrWhiteSpace(VirtualPathUtility.GetFileName(virtualPath)))
+            {
+                return;
+            }
 
             var physicalPath = Server.MapPath(virtualPath);
 
-            if (File.Exists(physicalPath) || Directory.Exists(physicalPath))
-            {
-                string newpath = string.Format("/dbfiles{0}", path);
+            if (!File.Exists(physicalPath)) return;
 
-                Trace.TraceInformation("Rewriting path from '{0}' to '{1}", path, newpath);
-                Context.RewritePath(newpath);
-            }
+            string newpath = string.Format("{0}{1}", cfgFolder.Substring(1), path);
+
+            Trace.TraceInformation("Rewriting path from '{0}' to '{1}", path, newpath);
+            Context.RewritePath(newpath);
         }
 
         protected void Application_AuthenticateRequest(object sender, EventArgs e)
