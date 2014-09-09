a66982baaacc9b649853eb01db715e0c815db43c
jpolvora <jpolvora@gmail.com>
2014-09-09 09:16:23 -0400

    tracing

--- HostWebApp/App_Start/RouteConfig.cs
+++ HostWebApp/App_Start/RouteConfig.cs
@@ -24,12 +24,6 @@ namespace HostWebApp
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-
-            routes.MapRoute(
-                name: "CatchAll",
-                url: "{*url}",
-                defaults: new { controller = "home", action = "catchall" });
-
         }
     }
 }
