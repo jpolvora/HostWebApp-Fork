98af2c1b65dcfa09ab6aaf5223f43d61c6aad3b6
jpolvora <jpolvora@gmail.com>
2014-09-09 16:46:08 -0400

    Squashed 'MvcLib/' content from commit 20ff7aa
    git-subtree-dir: MvcLib
    git-subtree-split: 20ff7aa4a74a4f3a642add1268a8b05a24192358

---
+++ MvcLib.Common/Configuration/HttpModulesElement.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+
+namespace MvcLib.Common.Configuration
+{
+    public class HttpModulesElement : ConfigurationElement
+    {
+        [ConfigurationProperty("trace")]
+        public TraceElement Trace
+        {
+            get { return (TraceElement)this["trace"]; }
+            set { this["trace"] = value; }
+        }
+
+        [ConfigurationProperty("customerror")]
+        public CustomErrorElement CustomError
+        {
+            get { return (CustomErrorElement)this["customerror"]; }
+            set { this["customerror"] = value; }
+        }
+
+        [ConfigurationProperty("whitespace")]
+        public WhiteSpaceElement WhiteSpace
+        {
+            get { return (WhiteSpaceElement)this["whitespace"]; }
+            set { this["whitespace"] = value; }
+        }
+    }
+}
