0c5f2d63a8219ce1f5b3b8159202719fad0bc5ed
jpolvora <jpolvora@gmail.com>
2014-09-12 13:47:27 -0400

    new helper

---
+++ Frankstein/Frankstein.Common.Mvc/WebPagesHtmlHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.WebPages;
+using System.Web.WebPages.Html;
+using Frankstein.Common.Configuration;
+
+namespace Frankstein.Common.Mvc
+{
+    public static class WebPagesHtmlHelpers
+    {
+        private static readonly string BasePath = BootstrapperSection.Instance.DumpToLocal.Folder;
+
+        public static string MapPath(this WebPage page, string relativePath)
+        {
+            var result = string.Format("{0}/{1}", BasePath.TrimEnd('/'), relativePath.TrimStart('~', '/'));
+            return result;
+        }
+    }
+}
