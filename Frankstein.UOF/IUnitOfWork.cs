003b6986fe223fce3e71a65108fe691c10028885
jpolvora <jpolvora@gmail.com>
2014-09-17 21:23:56 -0400

    Squashed 'Frankstein/' changes from bd650b6..d12be32
    d12be32 Added new projects
    
    git-subtree-dir: Frankstein
    git-subtree-split: d12be32972264e1969f32ff5ebf7d536e046dad9

---
+++ Frankstein.UOF/IUnitOfWork.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UOF
+{
+    public enum UnitOfWorkStatus
+    {
+        None,
+        Success,
+        Fail
+    }
+
+    public interface IUnitOfWork : IDisposable
+    {
+        UnitOfWorkStatus Status { get; }
+        bool Commit();
+        void RollBack();
+    }
+
+    public class UnitOfWorkBase : IUnitOfWork
+    {
+        public void Dispose()
+        {
+            RollBack();
+        }
+
+        public Exception Exception { get; private set; }
+
+        public UnitOfWorkStatus Status { get; private set; }
+
+        protected Queue<Action<Action>> Actions = new Queue<Action<Action>>();
+
+        public bool Commit()
+        {
+            if (Status == UnitOfWorkStatus.None)
+            {
+                try
+                {
+                    CommitWork();
+                    Status = UnitOfWorkStatus.Success;
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Exception = ex;
+                    Status = UnitOfWorkStatus.Fail;
+                    return false;
+                }
+            }
+
+            return Status == UnitOfWorkStatus.Success;
+        }
+
+        public virtual void CommitWork()
+        {
+            Action<Action> action = null;
+            while ((action = Actions.Dequeue()) != null)
+            {
+                action.Invoke(() => { });
+            }
+        }
+
+        public void RollBack()
+        {
+            if (Status == UnitOfWorkStatus.Fail)
+            {
+
+            }
+        }
+
+        public virtual void RollBackWork()
+        {
+
+        }
+    }
+
+    public class DbContextUnitOfWork : UnitOfWorkBase
+    {
+        public DbContextUnitOfWork(Func<int> saveChangesDelegate)
+        {
+
+        }
+    }
+}
