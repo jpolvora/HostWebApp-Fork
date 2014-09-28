19f17f0c82be5a7d2f68da893a4004034285434e
jpolvora <jpolvora@gmail.com>
2014-09-27 23:57:06 -0400

    Squashed 'Frankstein/' changes from 5135081..b65bb1e
    b65bb1e dbmodule fields
    
    git-subtree-dir: Frankstein
    git-subtree-split: b65bb1e23cdb63920badac4364c60a7bc451bdef

--- Frankstein.DbFileSystem/DbModule.cs
+++ Frankstein.DbFileSystem/DbModule.cs
@@ -1,10 +1,13 @@
-﻿using Frankstein.EntityFramework;
+﻿using System.ComponentModel.DataAnnotations;
+using Frankstein.EntityFramework;
 
 namespace Frankstein.DbFileSystem
 {
     public class DbModule : AuditableEntity<int>
     {
+        [StringLength(150)]
         public string Name { get; set; }
         public byte[] Zip { get; set; }
+        public byte[] CompiledAssembly { get; set; }
     }
 }
